4fed40a62e6f2402fca138604c632c3e23a88dec
SCHMITT Kilian <k.schmitt@altrans.fr>
2025-04-15 14:44:12 +0200

    Added try statement to windowname setting so it's more stable

--- changeWindowName.py
+++ changeWindowName.py
@@ -2,5 +2,8 @@ import win32gui
 
 
 def set_window_name(text):
-    hwnd = win32gui.GetForegroundWindow()
-    win32gui.SetWindowText(hwnd, text)
+    try:
+        hwnd = win32gui.GetForegroundWindow()
+        win32gui.SetWindowText(hwnd, text)
+    except:
+        pass
